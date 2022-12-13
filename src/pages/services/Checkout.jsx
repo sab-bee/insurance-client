6280e2c54579c530bbf5f01b01f31b2a3a0780d0
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-13 13:25:32 +0600

    plan modal

--- src/pages/services/Checkout.jsx
+++ src/pages/services/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { axiosPublic } from '../../api/axiosPublic';
@@ -6,84 +6,87 @@ import { auth } from '../../auth/firebase.init';
 import Spinner from '../../components/Spinner';
 import Policy from './Policy';
 import _ from 'lodash'
+import { AnimatePresence } from "framer-motion"
 
 const Checkout = ({ service }) => {
-  const [policy, setpolicy] = useState({})
+  const [policy, setPolicy] = useState({})
   const { title, premium, returns, desc, coverage, _id } = service
-  const { register, formState: { errors }, handleSubmit, reset } = useForm({ mode: 'onChange' });
+  const { register, formState: { errors }, handleSubmit } = useForm({ mode: 'onChange' });
   const [user] = useAuthState(auth)
+
   const onSubmit = (data) => {
     const email = user?.email
-    reset()
-    axiosPublic.post(`/insurances/${email}`, data).then((res) => setpolicy(res.data))
+    const name = user?.displayName
+    axiosPublic.post(`/insurances/${email}`, { ...data, name, service: title }).then((res) => {
+      setPolicy(res.data)
+    })
   }
 
   return (
-    <div className='w-3/4 mx-auto py-8'>
-      <h2 className='text-center text-2xl font-semibold'>{title}</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className='lg:w-1/5 md:w-2/5 sm:w-1/2 mx-auto space-y-4 my-12'>
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="name" className='font-medium'>Name</label>
-          <input value={user.displayName} readOnly className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="text" {
-            ...register('userName')
-          } />
-        </div>
+    <>
+      <div className='w-3/4 mx-auto py-8'>
+        <h2 className='text-center text-2xl font-semibold capitalize'>{title}</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className='lg:w-1/5 md:w-2/5 sm:w-1/2 mx-auto space-y-4 my-12'>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="name" className='font-medium'>Name</label>
+            <input value={user.displayName} disabled className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="text" />
+          </div>
 
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="age" className='font-medium'>
-            <span className='flex'>
-              <span className=''>age</span>
-              {errors.age && <p className='text-red-500'>*</p>}
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="userAge" className='font-medium'>
+              <span className='flex'>
+                <span>age</span>
+                {errors.userAge && <p className='text-red-500'>*</p>}
+                {
+                  (errors.userAge?.type === 'min' || errors.userAge?.type === 'max') && <p className='text-red-500 text-xs'>between 25-60 y/o</p>
+                }
+              </span>
+            </label>
+
+            <input className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="number" placeholder='enter your age'
               {
-                (errors.age?.type === 'min' || errors.age?.type === 'max') && <p className='text-red-500 text-xs'>between 25-60 y/o</p>
+              ...register('userAge', {
+                required: true,
+                min: 20,
+                max: 60,
+              })
               }
+            />
+
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="service" className='font-medium'>Service</label>
+            <input value={title} disabled className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="text" />
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="service" className='font-medium'>Coverage</label>
+            <select className='p-2 min-h-10 md:min-h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded'
+
+              {
+              ...register('coverage')
+              }>
+              {
+                coverage.map((cov, i) => <option value={cov} key={i}>{cov.toLocaleString()}</option>)
+              }
+            </select>
+          </div>
+
+
+          <button className='btn-primary w-full rounded h-10 md:h-9'>
+            <span className='flex justify-center'>
+              <Spinner /> get policy
             </span>
-
-          </label>
-
-          <input className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="number" placeholder='enter your age'
-            {
-            ...register('userAge', {
-              required: true,
-              min: 20,
-              max: 60,
-            })
-            }
-          />
-
-        </div>
-
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="service" className='font-medium'>Service</label>
-          <input value={title} readOnly className='p-2 h-10 md:h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded' type="text" {
-            ...register('service')
-          } />
-        </div>
-
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="service" className='font-medium'>Coverage</label>
-          <select className='p-2 min-h-10 md:min-h-9 border-2 outline-none focus:border-zinc-400 transition-colors duration-300 rounded'
-
-            {
-            ...register('coverage')
-            }>
-            {
-              coverage.map((cov, i) => <option value={cov} key={i}>{cov.toLocaleString()}</option>)
-            }
-          </select>
-        </div>
-
-
-        <button className='btn-primary w-full rounded h-10 md:h-9'>
-          <span className='flex justify-center'>
-            <Spinner /> get policy
-          </span>
-        </button>
-      </form>
-      {
-        _.isEmpty(policy) || <Policy policy={policy} />
-      }
-    </div>
+          </button>
+        </form>
+      </div>
+      <AnimatePresence>
+        {
+          _.isEmpty(policy) || <Policy policy={policy} setPolicy={setPolicy} />
+        }
+      </AnimatePresence>
+    </>
   )
 }
 
--- src/pages/services/Policy.jsx
+++ src/pages/services/Policy.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+
 import { RiShieldCheckFill } from "react-icons/ri";
 import { BsPinAngleFill } from "react-icons/bs";
+import { motion } from "framer-motion"
 
-const Policy = ({ policy }) => {
+const Policy = ({ policy, setPolicy }) => {
   const { service, premium, returns, desc } = policy
+
   return (
-    <div className='md:w-[600px] mx-auto'>
-      <h2 className='text-xl font-medium text-center'>Policy for this Serivce</h2>
-      <div className='shadow-lg shadow-zinc-100 p-8 rounded-lg border-2 mt-10 font-medium'>
+    <motion.div className="w-full flex items-center justify-center h-screen fixed top-0 backdrop-blur-sm bg-[rgba(0,0,0,0.08)]"
+      onClick={() => setPolicy({})}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3, ease: 'easeOut' }}
+      exit={{ opacity: 0 }}
+    >
+      <motion.div key='modal' className='w-4/5 md:w-[500px] min-h-[250px] bg-white p-8 rounded-xl border border-primary mt-10 font-medium'
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.3, ease: 'easeOut' }}
+        exit={{ scale: 0.3, opacity: 0 }}>
+        <h2 className='text-xl font-medium text-center mb-2'>Policy for this Serivce</h2>
         <p className='mb-2'>You have choose <span className='border-b-2 border-primary capitalize'>{service}</span> service</p>
 
-        <Points point={`you have to pay $${premium}/month premium`} />
+        <Points point={`premium $${premium}/month`} />
         <Points point={`${returns}`} />
         <Points point={`${desc}`} />
 
@@ -19,9 +31,9 @@ const Policy = ({ policy }) => {
           <span className='text-sm text-zinc-400 underline'>premium varies according to age and amount of coverage</span>
         </p>
 
-      </div>
-
-    </div>
+        <button className="btn-primary w-full mt-4">get subscribtion</button>
+      </motion.div>
+    </motion.div>
   )
 }
 
