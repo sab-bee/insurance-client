2676dc4459978e5ea791b5fe00222ce626bf6505
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-16 02:29:05 +0600

    working

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from 'react-router-dom'
 const CustomLink = ({ children, to }) => {
   const { pathname } = useLocation()
   return (
-    <Link to={to} className={`${pathname === to && 'text-primary'} hover:text-primary font-medium transition-colors ease-linear`} >{children}</Link>
+    <Link to={to} className={`${pathname === to && 'text-primary'} hover:text-primary font-medium transition-all ease-linear`} >{children}</Link>
   )
 }
 
